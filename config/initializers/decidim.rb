5e3d5062a3acf6d2e8fafd179601b301ab4362c6
Xavier <xavier.redo@marsbased.com>
2018-01-15 15:22:46 +0100

    Change email sender so it can be configured via secrets
    In different environments it will have different credentials

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -2,7 +2,7 @@
 
 Decidim.configure do |config|
   config.application_name = "Decidim Rubi"
-  config.mailer_sender = "change-me@domain.org"
+  config.mailer_sender = Rails.application.secrets.mailer_sender
 
   # Change these lines to set your preferred locales
   config.default_locale = :ca
