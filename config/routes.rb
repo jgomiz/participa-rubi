de599b159ae7b41dbd604023ad2bd5e2d4de5530
Fernando Blat <fernando@blat.es>
2022-06-28 06:13:03 +0200

    Add letter opener route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  if Rails.env.development? || Rails.env.staging?
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
+
   mount Decidim::Core::Engine => "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
