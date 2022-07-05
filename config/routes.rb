2fb25293af149104d932e70e374ca5348579e427
Fernando Blat <fernando@blat.es>
2022-07-05 07:57:26 +0200

    Fix rails route

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  if Rails.env.staging?
-    mount LetterOpenerWeb::Engine, at: '/letter_opener'
-  end
-
   mount Decidim::Core::Engine => "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
